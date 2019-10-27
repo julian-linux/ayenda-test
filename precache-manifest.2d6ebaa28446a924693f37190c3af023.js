bc2c7add5389a67d5de2546e71060beba5a7e5de
Julian Mosquera <julian.linux@gmail.com>
2019-10-27 01:02:17 -0500

    Updates

---
+++ precache-manifest.2d6ebaa28446a924693f37190c3af023.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af9e8c8be7131eb69836e08810492821",
+    "url": "/index.html"
+  },
+  {
+    "revision": "47fb3f42a2a8341f9104",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "e282179d147c13fd9204",
+    "url": "/static/css/3.5f640752.chunk.css"
+  },
+  {
+    "revision": "a7af5d64b2a856e0f562",
+    "url": "/static/css/4.5f640752.chunk.css"
+  },
+  {
+    "revision": "86aa6682904a2394d267",
+    "url": "/static/css/main.96611fb1.chunk.css"
+  },
+  {
+    "revision": "47fb3f42a2a8341f9104",
+    "url": "/static/js/2.2abafff2.chunk.js"
+  },
+  {
+    "revision": "e282179d147c13fd9204",
+    "url": "/static/js/3.1f938cf1.chunk.js"
+  },
+  {
+    "revision": "a7af5d64b2a856e0f562",
+    "url": "/static/js/4.5f5ee95f.chunk.js"
+  },
+  {
+    "revision": "071ff164e0eb86589687",
+    "url": "/static/js/5.aa045738.chunk.js"
+  },
+  {
+    "revision": "586c09d26f2d1be7eb9a",
+    "url": "/static/js/6.52a8ea70.chunk.js"
+  },
+  {
+    "revision": "3eed1e3f533a6399536e",
+    "url": "/static/js/7.d3c33a25.chunk.js"
+  },
+  {
+    "revision": "86aa6682904a2394d267",
+    "url": "/static/js/main.25a89277.chunk.js"
+  },
+  {
+    "revision": "5136eccc60875ac2dbd7",
+    "url": "/static/js/runtime-main.c284fb47.js"
+  },
+  {
+    "revision": "d98f34750ef6f37047aa2536821f258b",
+    "url": "/static/media/light-bulb.d98f3475.svg"
+  },
+  {
+    "revision": "cb750aeff95ed6e13a93d8afd5350489",
+    "url": "/static/media/play.cb750aef.svg"
+  }
+]);
